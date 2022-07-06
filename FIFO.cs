593ee856ba9c1d5823452e4fa3ebaf5bb7b319ea
Alexssandro Rodrigues Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2022-07-06 19:50:57 -0300

    Add files via upload

---
+++ FIFO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class QueueExample
+{
+    public static void Main(String[] args)
+    {
+        Queue<int> q = new Queue<int>();
+
+        var watch = new System.Diagnostics.Stopwatch();
+
+        watch.Start();
+
+        //Adicionar elementos na fila
+        for (int i = 0; i < 10000000; i++)
+            q.Enqueue(i);
+
+        // Display contents of the queue.
+        Console.Write("Elementos da FILA-");
+        foreach (int s in q) Console.Write(s + " ");
+
+        watch.Stop();
+
+       	Console.WriteLine($"Tempo de execução: {watch.ElapsedMilliseconds} ms");
+
+      	int size = q.Count;
+        Console.WriteLine("\nTamanho da fila -" + size);
+    }
+
+}
---
+++ LIFO.cs
@@ -0,0 +1,53 @@
+﻿// C# program to demonstrate
+// working of LIFO
+// using Stack in C#
+using System;
+using System.Collections.Generic;
+
+class LIFO
+{
+    // Pushing element on the top of the stack
+    static void stack_push(Stack<int> stack)
+    {
+        for (int i = 0; i < 10000000; i++)
+        {
+            stack.Push(i);
+        }
+    }
+
+    // Popping element from the top of the stack
+    static void stack_pop(Stack<int> stack)
+    {
+        Console.WriteLine("Pop :");
+
+        for (int i = 0; i < 10000000; i++)
+        {
+            int y = (int)stack.Pop();
+            Console.WriteLine(y);
+        }
+    }
+
+    // Displaying element on the top of the stack
+    static void stack_peek(Stack<int> stack)
+    {
+        int element = (int)stack.Peek();
+        Console.WriteLine("Element on stack top : " + element);
+    }
+    
+
+    // Driver code
+    public static void Main(String[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+
+        stack_push(stack);
+        stack_pop(stack);
+        stack_push(stack);
+        stack_peek(stack);
+        watch.Stop();
+        Console.WriteLine($"\n Tempo de execução: {watch.ElapsedMilliseconds} ms");
+    }
+}
